14526a98fcb7de9025dfdb12d470040717e25cd3
Kitty Samsonadze <kettigress@gmail.com>
2022-07-25 23:14:55 +0400

    with JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,4 @@
 
-
-
-// const submitButton = document.querySelector ('.pop-up__button-submit');
-
-// const editPopup = document.querySelector ('.profile__edit-button');
-// const popup = document.getElementsByClassName ('pop-up');
-// const popupOpened = document.getElementsByClassName ('pop-up__opened');
-// let popupName = document.querySelector('.pop-up__field-name');
-// let popupBio = document.querySelector('.pop-up__field-bio');
-// let nameEdited;
-// let bioEdited;
-
-
-
-// function openPopup(popup) {
-//     popup[0].classList.toggle('pop-up__opened');
-//     }
-
-// editPopup.addEventListener('click', () => openPopup(popup));
-
-
-
-
-
 let closeButton = document.querySelector ('.button__close');
 let closePopupButton = document.querySelector ('.pop-up__button-close');
 let editPopup = document.querySelector ('.profile__edit-button');
@@ -46,7 +22,8 @@ function closePopup() {
     document.getElementsByClassName ('pop-up')[0].classList.toggle('pop-up__opened');
 }
 
-let formElement = document.getElementsByClassName('pop-up__submitform');
+const formElement = document.querySelector('.pop-up__submitform');
+
 console.log(formElement);
 
 function formSubmitHandler(evt) {
